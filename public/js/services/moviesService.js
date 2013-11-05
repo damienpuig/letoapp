da0d5a80c53d9a52239da1a11dc361cb05e8decb
Damien PUIG <damien.puig@gmail.com>
2013-11-05 12:58:13 +0000

    movie service init

--- public/js/services/moviesService.js
+++ public/js/services/moviesService.js
@@ -3,6 +3,17 @@
  */
 //declaration of services in order to make request to the server / other sources
 leto.service('moviesService', ['$http', function ($http) {
+    return {
+      all: function(callback){
+
+      },
+      get: function(pid, callback){
+
+      },
+      getrating: function(novieName, callback){
+
+      }
+    };
 }]);
 
 
