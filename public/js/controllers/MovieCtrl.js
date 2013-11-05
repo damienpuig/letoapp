6744e53ac6d5af76f97b2b27155972f3a7031186
Damien PUIG <damien.puig@gmail.com>
2013-11-05 15:05:30 +0000

    add some comments to the sources

--- public/js/controllers/MovieCtrl.js
+++ public/js/controllers/MovieCtrl.js
@@ -7,19 +7,25 @@
 leto.controller('MoviesCtrl', ['$scope', '$routeParams', 'moviesService', function ($scope, $routeParams, moviesService) {
     $scope.movies;
 
+    // the getrating object will fill each movie object by requesting the right movie on themoviedb.
+    //I assume that the right result if the first result of the list.
     var getrating = function () {
         $scope.movies.episodes.forEach(function (movie) {
-            moviesService.getrating(movie.programme.title, function (data) {
 
+            moviesService.getrating(movie.programme.title, function (data) {
                 movie.hasrating = '';
 
                 if (data.results.length > 0) {
 
                     movie.hasrating = 'true';
 
+                    //I assume that the first object of the list is the right result...
                     movie.tmdb = data['results'][0];
                     movie.tmdb.ratings = [];
 
+                    //I affect stars depending on the average of the vote ( I 'floor' the result because I didn't use the ressource 'star-half.png')
+                    //The ressources come from themovieDB website.
+                    // I assume that the mark is always /10.
                     for (var mark = 0; mark < 10 ; mark++){
                         if(mark < Math.floor(movie.tmdb.vote_average)) {
                             movie.tmdb.ratings.push("https://d3a8mw37cqal2z.cloudfront.net/images/star-on.png");
@@ -50,8 +56,10 @@ leto.controller('MovieCtrl', ['$scope', '$routeParams', 'moviesService', functio
     $scope.movie;
     $scope.properties = new Array();
 
+    //pid of the selected movie
     var pid = $routeParams.pid;
 
+    // this method format, in a array, each property of a movie following the pattern "object.property = value"
     var format = function (obj, objName) {
         for ( var prop in obj) {
             if (obj.hasOwnProperty(prop)) {
@@ -62,8 +70,11 @@ leto.controller('MovieCtrl', ['$scope', '$routeParams', 'moviesService', functio
 
     moviesService.get(pid, function (movie) {
         $scope.movie = movie.programme;
+
+        //little hack to load dynamically the picture.
         $scope.movie.image.url = 'http://ichef.bbci.co.uk/images/ic/272x153/'+ $scope.movie.image.pid + '.jpg';
 
+        //creation of an array containing each formatted properties.
         format(movie.programme, movie.programme.constructor.name);
 
         $scope.$apply();
--- public/js/services/moviesService.js
+++ public/js/services/moviesService.js
@@ -4,6 +4,7 @@
 //declaration of services in order to make request to the server / other sources
 leto.service('moviesService', ['$http', function ($http) {
     return {
+      //return a list of movies, json result.
       all: function(callback){
           $.ajax({
                   type:'GET',
@@ -12,6 +13,7 @@ leto.service('moviesService', ['$http', function ($http) {
                       callback(data);
           }});
       },
+       //Use the pid of a movie to get it details following the bbc website, json result.
       get: function(pid, callback){
           $.ajax({
               type:'GET',
@@ -21,6 +23,8 @@ leto.service('moviesService', ['$http', function ($http) {
               }
           });
       },
+
+      //Use the movie name of a movie to retreive a json result containing the vote_average and vote_count.
       getrating: function (movieName, callback){
           var option = {
               url: 'http://api.themoviedb.org/3/',
