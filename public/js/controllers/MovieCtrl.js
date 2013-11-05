3c8d1ae013b7504f8f7300d97a887ec24ca2bbe3
Damien PUIG <damien.puig@gmail.com>
2013-11-05 12:42:05 +0000

    angular init architecture

--- public/js/controllers/MovieCtrl.js
+++ public/js/controllers/MovieCtrl.js
@@ -3,3 +3,15 @@
  */
 // controller handling interactions with the views
 
+//controller handling the movie list
+leto.controller('MoviesCtrl', ['$scope', '$routeParams', 'moviesService', function ($scope, $routeParams, moviesService) {
+    $scope.movies;
+}]);
+
+
+//controller handling a single movie detail
+// pid is the id of the current movie
+leto.controller('MovieCtrl', ['$scope', '$routeParams', 'moviesService', function ($scope, $routeParams, moviesService) {
+    $scope.movie;
+    var pid = $routeParams.pid;
+}]);
--- public/js/services/movieService.js
+++ public/js/services/movieService.js
@@ -2,3 +2,7 @@
  * Created by Damien on 05/11/13.
  */
 //declaration of services in order to make request to the server / other sources
+leto.service('moviesService', ['$http', function ($http) {
+}]);
+
+
